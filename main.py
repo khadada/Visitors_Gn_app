7c6c068fcc444b9e36e5ecb3c139db13968250a6
khadada <lkhadada@gmail.com>
2021-12-07 07:21:26 +0100

    I create window of the app with title

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
 from tkinter import *
 # Variabes ------------------------
 # Window --------------------------
+window=Tk()
+window.title('Visitors GN Temacine')
 # Header --------------------------
 # Container -----------------------
 # Footer -----------------------
+
+
+
+
+
+
+window.mainloop()
